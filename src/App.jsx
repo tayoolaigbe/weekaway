e951549ce52f9bd4476a9385e2240bbcf2e782b7
tayoolaigbe <tayoolaigbe@outlook.com>
2023-03-29 22:55:22 +0100

    FImished configuring tailwindcss and created the TopBar component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const App = () => {
 	return (
-		<div className="text-6xl text-purple-600 hover:text-green-200 font-bold underline">
+		<div className="text-6xl text-purple-900 hover:text-green-200 font-bold underline">
 			App
 		</div>
 	);
