726463292d0587b9caebf60ca7ef47ae52129274
tayoolaigbe <tayoolaigbe@outlook.com>
2023-03-29 10:21:30 +0100

    Completed the tailwindcss customisation in index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const App = () => {
-	return <div className="text-6xl font-bold underline">App</div>;
+	return (
+		<div className="text-6xl text-purple-600 hover:text-green-200 font-bold underline">
+			App
+		</div>
+	);
 };
 
 export default App;
