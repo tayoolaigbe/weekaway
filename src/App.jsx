1e2cff3c2cfdad5a67a75c7db231ddf8171fdbba
tayoolaigbe <tayoolaigbe@outlook.com>
2023-03-30 21:00:59 +0100

    Completed the Hero.jsx component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 import Topbar from './components/Topbar';
 
@@ -7,6 +8,7 @@ const App = () => {
 		<div>
 			<Topbar />
 			<Navbar />
+			<Hero />
 		</div>
 	);
 };
